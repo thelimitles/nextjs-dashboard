3815b253d1434dc1dfb216705dc15bc1d114f55a
wahidrizka <wahidrizka.0310@gmail.com>
2023-11-12 22:37:22 +0700

    initial commit :rocket:

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+import "@/app/ui/global.css";
+import { outfit } from "@/app/ui/fonts";
+
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang="en">
+			<body className={`${outfit.className} antialiased`}>{children}</body>
+		</html>
+	);
+}
