3fc03c26fd78c817742df975b262130b9eab7b5e
wahidrizka <58039870+wahidrizka@users.noreply.github.com>
2023-11-13 12:04:01 +0700

    Chapter 7 - Chapter 11:kissing:
    - Fetching data
    - Static and Dynamic Rendering
    - Streaming
    - Partial Prerendering
    - Adding Search and Pagination

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -1,28 +1,27 @@
 import { ArrowPathIcon } from "@heroicons/react/24/outline";
 import clsx from "clsx";
 import Image from "next/image";
-import { LatestInvoice } from "@/app/lib/definitions";
-export default async function LatestInvoices({
-	latestInvoices,
-}: {
-	latestInvoices: LatestInvoice[];
-}) {
-	return (
-		<div className="flex w-full flex-col md:col-span-4 lg:col-span-4">
-			<h2 className="mb-4 text-xl md:text-2xl">Latest Invoices</h2>
-			<div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
-				{/* NOTE: comment in this code when you get to this point in the course */}
+import { fetchLatestInvoices } from "@/app/lib/data";
 
-				{/* <div className="bg-white px-6">
+export default async function LatestInvoices() {
+  const latestInvoices = await fetchLatestInvoices();
+
+  return (
+    <div className="flex w-full flex-col md:col-span-4 lg:col-span-4">
+      <h2 className="mb-4 text-xl md:text-2xl">Latest Invoices</h2>
+      <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
+        {/* NOTE: comment in this code when you get to this point in the course */}
+
+        <div className="bg-white px-6">
           {latestInvoices.map((invoice, i) => {
             return (
               <div
                 key={invoice.id}
                 className={clsx(
-                  'flex flex-row items-center justify-between py-4',
+                  "flex flex-row items-center justify-between py-4",
                   {
-                    'border-t': i !== 0,
-                  },
+                    "border-t": i !== 0,
+                  }
                 )}
               >
                 <div className="flex items-center">
@@ -42,20 +41,18 @@ export default async function LatestInvoices({
                     </p>
                   </div>
                 </div>
-                <p
-                  className="truncate text-sm font-medium md:text-base"
-                >
+                <p className="truncate text-sm font-medium md:text-base">
                   {invoice.amount}
                 </p>
               </div>
             );
           })}
-        </div> */}
-				<div className="flex items-center pb-2 pt-6">
-					<ArrowPathIcon className="h-5 w-5 text-gray-500" />
-					<h3 className="ml-2 text-sm text-gray-500 ">Updated just now</h3>
-				</div>
-			</div>
-		</div>
-	);
+        </div>
+        <div className="flex items-center pb-2 pt-6">
+          <ArrowPathIcon className="h-5 w-5 text-gray-500" />
+          <h3 className="ml-2 text-sm text-gray-500 ">Updated just now</h3>
+        </div>
+      </div>
+    </div>
+  );
 }
