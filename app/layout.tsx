e0e75765f59cf1c0153659fa3cc42213fc26af0e
wahidrizka <58039870+wahidrizka@users.noreply.github.com>
2023-11-14 13:12:15 +0700

    Add metadata:sleepy:

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,25 @@
 import "@/app/ui/global.css";
 import { outfit } from "@/app/ui/fonts";
 
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Acme Dashboard",
+    default: "Acme Dashboard",
+  },
+  description: "The official Next.js Learn Dashboard built with App Router.",
+  metadataBase: new URL("https://next-learn-dashboard.vercel.sh"),
+};
+
 export default function RootLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-	return (
-		<html lang="en">
-			<body className={`${outfit.className} antialiased`}>{children}</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body className={`${outfit.className} antialiased`}>{children}</body>
+    </html>
+  );
 }
